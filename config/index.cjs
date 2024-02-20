528e04acb50e26bd9532ee9a84915eef9e846f49
wuzhongmiwu <125187191+wuzhongmiwu@users.noreply.github.com>
2024-02-20 21:30:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,9 +22,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小申',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'cf74d3ec57834aaeb8f435741cd8ab96',
+      id: 'oBiA963__ZS9Yge0OoHCdZLr7hHQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'T_9_CmJTn_DlzQvWIbdGlBrvmgamGBaQEDDXyHlqICw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-23',
       festivals: [
