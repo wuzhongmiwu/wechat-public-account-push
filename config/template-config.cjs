73707194be5aa7a27969becbeba0d1a62d4a7785
wuzhongmiwu <125187191+wuzhongmiwu@users.noreply.github.com>
2024-02-20 21:15:00 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -35,23 +35,7 @@ const TEMPLATE_CONFIG = [
       {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
   },
-  {
-    id: '0002',
-    title: '推送完成提醒',
-    desc: `
-      服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
-      
-      ---
-      
-      共推送 {{need_post_num.DATA}} 人
-      
-      成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-      
-      成功用户: {{success_post_ids.DATA}}
-      
-      失败用户: {{fail_post_ids.DATA}}
-    `
-  },
+  
 ]
 
 module.exports = TEMPLATE_CONFIG
